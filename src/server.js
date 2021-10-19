db790a7f6eec714ff807a2041304d0b735ccf72c
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-19 09:35:04 +0300

    static assets, sendFile

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path'); // node module
 
 // const mysql = require('mysql2/promise');
 // const dbConfig = require('./dbConfig');
@@ -14,8 +15,15 @@ app.use(morgan('common'));
 app.use(cors());
 app.use(express.json());
 
+// static directory for css, img, js front files
+const staticPath = path.join(__dirname, 'assets');
+// console.log('staticPath', staticPath);
+app.use(express.static(staticPath));
+
 app.get('/', (req, res) => {
-  res.send('Hello express');
+  // res.send('<h1>Welcome to Ejs</h1>');
+  // send file as a response
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 const sampleRoutes = require('./routes/v1/sampleRoute');
