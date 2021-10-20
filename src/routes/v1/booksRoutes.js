d8838b47093c499036770fb7de901b83c937bf18
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-20 09:42:26 +0300

    list all books

--- src/routes/v1/booksRoutes.js
+++ src/routes/v1/booksRoutes.js
@@ -6,7 +6,12 @@ const router = express.Router();
 // GET /books/ - get all books
 router.get('/', async (req, res) => {
   // 1. get all the data
-  const sql = 'SELECT * FROM books';
+  const sql = `
+  SELECT books.id, books.title, books.author, books.timeStamp, books.year, book_categories.cat_name AS category
+  FROM books
+  LEFT JOIN book_categories
+  ON book_categories.id = books.category
+  `;
   const dbResult = await dbAction(sql);
   if (dbResult === false) {
     return dbFail();
@@ -14,7 +19,11 @@ router.get('/', async (req, res) => {
   const books = dbResult;
   // res.json({ msg: 'all books', books });
   // 2. send response page with data
-  res.render('books/index', { books });
+  const data = {
+    title: 'Our books',
+    books,
+  };
+  res.render('books/index', data);
 });
 
 module.exports = router;
