12f13d37879c190c50e1797f3a740250e0d3f373
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-21 09:00:17 +0300

    separate model from controller

--- src/controllers/booksControllers.js
+++ src/controllers/booksControllers.js
@@ -1,24 +1,22 @@
 // booksIndex, booksGetNew, booksPostNew
 
+const { getAllBooks } = require('../models/booksModels');
+
 const booksIndex = async (req, res) => {
+  const feedback = { msg: '', error: '' };
   // 1. get all the data
-  const sql = `
-SELECT books.id, books.title, books.author, books.timeStamp, books.year, book_categories.cat_name AS category
-FROM books
-LEFT JOIN book_categories
-ON book_categories.id = books.category
-`;
-  const dbResult = await dbAction(sql);
-  if (dbResult === false) {
-    return dbFail(res);
+  const books = await getAllBooks();
+
+  if (books === false) {
+    feedback.error = 'Error gettig books';
   }
-  const books = dbResult;
-  // res.json({ msg: 'all books', books });
+
   // 2. send response page with data
   const data = {
     title: 'Our books',
     currentPage: 'books',
     books,
+    feedback,
   };
   res.render('books/index', data);
 };
