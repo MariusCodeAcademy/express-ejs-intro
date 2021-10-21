d87484e6d2c69610578fd87d7d8645bc95e03cdc
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-21 11:06:41 +0300

    front end js example

--- src/assets/js/front.js
+++ src/assets/js/front.js
@@ -1,1 +1,11 @@
 console.log('front.js');
+
+// select elements
+const submitBtnEl = document.getElementById('add-book');
+const categorySelectEl = document.getElementById('category');
+
+submitBtnEl.setAttribute('disabled', true);
+
+categorySelectEl.addEventListener('change', (e) => {
+  submitBtnEl.removeAttribute('disabled');
+});
